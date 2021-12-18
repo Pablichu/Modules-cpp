e411ae60f8e0e5e6043761ad0d16b618a4b48aab
Pablichu <pablo200016@gmail.com>
2021-12-18 02:27:31 +0100

    Megaphone finished, I think

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -1,4 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pmira-pe <pmira-pe@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/18 02:16:57 by pmira-pe          #+#    #+#             */
+/*   Updated: 2021/12/18 02:18:11 by pmira-pe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-using namespace std;
+#include <string>
+#include <iostream>
 
-std::cout << "cojonudo";
+int	main(int argc, char **argv)
+{
+	if (argc > 1)
+	{
+		for (int j = 1; j < argc; j++)
+		{
+			for (int i = 0; argv[j][i]; i++)
+				std::cout << (char)std::toupper(argv[j][i]);
+			std::cout << " ";
+		}
+		std::cout << std::endl;
+	}
+	else
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	return 0;
+}
