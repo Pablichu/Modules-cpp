5de24dbb5b5e01f7d2909a3ad8b5590fa3210212
pmira-pe <pmira-pe@student.42madrid.com>
2021-12-22 00:13:32 +0100

    I made something

---
+++ cpp00/ex01/telephone.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   telephone.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pmira-pe <pmira-pe@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/21 23:55:51 by pmira-pe          #+#    #+#             */
+/*   Updated: 2021/12/22 00:08:54 by pmira-pe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef telephonehpp
+#define telefphonehpp
+
+#include <string>
+#include <iostream>
+
+class	contact {
+	std::string	name;
+	std::string	last_name;
+	std::string	nickname;
+	std::string	phone_nb;
+	std::string	dark_secret;
+}		contc;
+
+#endif
