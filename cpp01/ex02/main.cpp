793240b85c6dc4e3372c0c680c5e143213b2bbbe
Pablichu <pablo200016@gmail.com>
2022-01-14 00:34:49 +0100

    ex02 finished and beginin ex03

---
+++ cpp01/ex02/main.cpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pmira-pe <pmira-pe@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/14 00:08:10 by pmira-pe          #+#    #+#             */
+/*   Updated: 2022/01/14 00:08:10 by pmira-pe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int main(void)
+{
+	std::string	le_string = "HI THIS IS BRAIN";
+
+	std::string *stringPTR = &le_string;
+	std::string &stringREF = le_string;
+
+	std::cout << "Original: " << &le_string << std::endl
+			  << "StringPTR: " << stringPTR << std::endl
+			  << "StringREF: " << &stringREF << std::endl << std::endl;
+
+	std::cout << "StringPTR: " << *stringPTR << std::endl
+			  << "StringREF: " << stringREF << std::endl;
+}
