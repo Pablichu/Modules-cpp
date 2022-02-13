4654bb07173ad7ec6e9f9817c19450caf1837182
pmira-pe <pmira-pe@student.42madrid.com>
2022-02-13 22:50:11 +0100

    minor change

--- cpp04/ex00/main.cpp
+++ cpp04/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: pmira-pe <pmira-pe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/30 20:49:10 by pmira-pe          #+#    #+#             */
-/*   Updated: 2022/02/10 19:25:08 by pmira-pe         ###   ########.fr       */
+/*   Updated: 2022/02/13 20:57:00 by pmira-pe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,7 @@ int	main()
 	delete(meta);
 	delete(i);
 	delete(j);
+	std::cout << std::endl << std::string(25, '-') << std::endl;
 
 	const WrongAnimal* diff = new WrongAnimal();
 	const WrongAnimal* x = new WrongCat();
