3d80270c6f43c64c4e27d469473350575d434ced
pmira-pe <pmira-pe@student.42madrid.com>
2023-03-08 20:46:51 +0100

    Beggining with ex02

--- cpp09/ex02/PmergeMe.cpp
+++ cpp09/ex02/PmergeMe.cpp
@@ -6,7 +6,29 @@
 /*   By: pmira-pe <pmira-pe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/03 22:01:23 by pmira-pe          #+#    #+#             */
-/*   Updated: 2023/03/03 22:01:43 by pmira-pe         ###   ########.fr       */
+/*   Updated: 2023/03/08 20:46:26 by pmira-pe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "PmergeMe.hpp"
+
+PmergeMe::PmergeMe()
+{
+}
+
+PmergeMe::PmergeMe(const char **arg_nbs)
+{
+}
+
+PmergeMe::PmergeMe(PmergeMe const &src)
+{
+}
+
+PmergeMe::~PmergeMe()
+{
+}
+
+PmergeMe &PmergeMe::operator=(PmergeMe const &src)
+{
+	// TODO: insert return statement here
+}
--- cpp09/ex02/PmergeMe.hpp
+++ cpp09/ex02/PmergeMe.hpp
@@ -6,7 +6,32 @@
 /*   By: pmira-pe <pmira-pe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/03 22:01:19 by pmira-pe          #+#    #+#             */
-/*   Updated: 2023/03/03 22:01:44 by pmira-pe         ###   ########.fr       */
+/*   Updated: 2023/03/08 20:27:28 by pmira-pe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <deque>
+
+class PmergeMe
+{
+	private:
+	std::vector<int>	cont1;
+	std::deque<int>		cont2;
+
+
+
+	size_t	nbrs;
+
+	public:
+		PmergeMe();
+		PmergeMe(const char **arg_nbs);
+		PmergeMe(PmergeMe const &src);
+		~PmergeMe();
+
+		PmergeMe &operator=(PmergeMe const &src);
+		
+};
